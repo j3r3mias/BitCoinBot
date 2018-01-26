cfb94c538b7405f9b61fdb09e59322c10eaa03ec
Alfredo Casanova <atcasanova@gmail.com>
2018-01-26 13:18:29 -0200

    more legibility on command listener

--- variaveis.sh
+++ variaveis.sh
@@ -8,3 +8,12 @@ LTCMAX="150"
 LTCMIN="111"
 PORCENTAGEM="4"
 last="oe"
+COMANDOS="^/cotacoes$|\
+^/[lb]tcm[ai][xn] [0-9]+$|\
+^/help$|^/parametros$|\
+^/intervalo [0-9]+(\.[0-9])?$|\
+^/porcentagem [0-9]{1,2}(\.[0-9]{1,2})?$|\
+^/coin( [a-zA-Z0-9.-]+){1,2}$|\
+^/adiciona [0-9a-zA-Z-]+ [0-9]+(\.[0-9]+)?$|\
+^/remove [0-9a-zA-Z-]+$|\
+^/consulta$"
